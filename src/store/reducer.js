5ab1dd00f7b9a34aa082869cf66f0a135a2cfb0d
Abhishek K <abhishekakade@gmail.com>
2019-04-06 21:11:13 +0530

    fix: AGE_DOWN now decrements

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -18,14 +18,14 @@ const reducer = (state = initialState, action) => {
     case "AGE_DOWN":
       return {
         ...state,
-        age: state.age + action.value,
-        history: state.history.concat({ id: Math.random(), age: state.age + action.value })
+        age: state.age - action.value,
+        history: state.history.concat({ id: Math.random(), age: state.age - action.value })
       };
       break;
     case "DEL_ENTRY":
       return {
         ...state,
-        history: newState.history.filter((el)=> el.id !== action.id )
+        history: newState.history.filter((el) => el.id !== action.id)
       }
       break;
   }
